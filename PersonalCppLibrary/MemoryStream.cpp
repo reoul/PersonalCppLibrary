119271e3bcf3a30e2c57d33852b810ad45db0004
Reoul <dydqja1358@gmail.com>
2022-09-07 21:05:09 +0900

    logger랑 MemoryStream 추가
    logger랑 MemoryStream 추가

---
+++ PersonalCppLibrary/MemoryStream.cpp
@@ -0,0 +1,73 @@
+﻿#include "pch.h"
+#include "MemoryStream.h"
+#include "logger.h"
+
+void WriteMemoryStream::Write(const std::string& inString)
+{
+	size_t elementCount = inString.size();
+	Write(elementCount);
+	Write(inString.data(), elementCount * sizeof(char));
+}
+
+void WriteMemoryStream::ReallocBuffer(uint32_t inNewLength)
+{
+	mBuffer = static_cast<char*>(std::realloc(mBuffer, inNewLength));
+	if(mBuffer == nullptr)
+	{
+		//log
+	}
+	mCapacity = inNewLength;
+}
+
+WriteMemoryStream::WriteMemoryStream()
+	: mBuffer(nullptr)
+	, mHead(0)
+	, mCapacity(0)
+{
+	ReallocBuffer(32);
+}
+
+WriteMemoryStream::~WriteMemoryStream()
+{
+	std::free(mBuffer);
+}
+
+void WriteMemoryStream::Write(const void* inData, size_t inByteCount)
+{
+	const uint32_t resultHead = mHead + static_cast<uint32_t>(inByteCount);
+	if (resultHead > mCapacity) // 공간 부족할 시
+	{
+		ReallocBuffer(std::max(mCapacity * 2, resultHead)); // 새 공간 할당
+	}
+	
+	std::memcpy(mBuffer + mHead, inData, inByteCount);
+	
+	mHead = resultHead;
+}
+
+
+ReadMemoryStream::ReadMemoryStream(char* inBuffer, uint32_t inByteCount)
+	: mBuffer(inBuffer)
+	, mHead(0)
+	, mCapacity(inByteCount)
+{
+}
+
+ReadMemoryStream::~ReadMemoryStream()
+{
+	std::free(mBuffer);
+}
+
+void ReadMemoryStream::Read(void* outData, uint32_t inByteCount)
+{
+	uint32_t resultHead = mHead + inByteCount;
+	if (resultHead > mCapacity)
+	{
+		//handle error, no data to read!
+		//...
+	}
+
+	std::memcpy(outData, mBuffer + mHead, inByteCount);
+
+	mHead = resultHead;
+}
---
+++ include/reoul/MemoryStream.h
@@ -0,0 +1,106 @@
+﻿#pragma once
+#include <string>
+
+class WriteMemoryStream
+{
+public:
+	WriteMemoryStream();
+	WriteMemoryStream(const WriteMemoryStream&) = delete;
+	~WriteMemoryStream();
+
+	//get a pointer to the data in the stream
+	inline const char* GetBufferPtr() const;
+	inline uint32_t GetLength() const;
+	void Write(const void* inData, size_t inByteCount);
+
+	template <typename T>
+	void Write(T inData);
+
+	/*void Write(const std::vector< int >& inIntVector)
+	{
+		const size_t elementCount = inIntVector.size();
+		Write(elementCount);
+		Write(inIntVector.data(), elementCount * sizeof(int));
+	}
+
+	template< typename T >
+	void Write(const std::vector< T >& inVector)
+	{
+		const uint32_t elementCount = inVector.size();
+		Write(elementCount);
+		for (const T& element : inVector)
+		{
+			Write(element);
+		}
+	}*/
+
+	void Write(const std::string& inString);
+
+private:
+	void ReallocBuffer(uint32_t inNewLength);
+
+	char* mBuffer;
+	uint32_t mHead;
+	uint32_t mCapacity;
+};
+
+class ReadMemoryStream
+{
+public:
+	ReadMemoryStream() = delete;
+	ReadMemoryStream(char* inBuffer, uint32_t inByteCount);
+	ReadMemoryStream(const ReadMemoryStream&) = delete;
+	~ReadMemoryStream();
+
+	inline uint32_t GetRemainingDataSize() const;
+	void Read(void* outData, uint32_t inByteCount);
+
+	template <typename T>
+	void Read(T& outData);
+
+	/*template< typename T >
+	void Read(std::vector< T >& outVector)
+	{
+		size_t elementCount;
+		Read(elementCount);
+		outVector.resize(elementCount);
+		for (const T& element : outVector)
+		{
+			Read(element);
+		}
+	}*/
+
+private:
+	char* mBuffer;
+	uint32_t mHead;
+	uint32_t mCapacity;
+};
+
+const char* WriteMemoryStream::GetBufferPtr() const
+{
+	return mBuffer;
+}
+
+uint32_t WriteMemoryStream::GetLength() const
+{
+	return mHead;
+}
+
+template <typename T>
+void WriteMemoryStream::Write(T inData)
+{
+	static_assert(std::is_arithmetic<T>::value || std::is_enum<T>::value, "Generic Write only supports primitive data types");
+	Write(&inData, sizeof(inData));
+}
+
+uint32_t ReadMemoryStream::GetRemainingDataSize() const
+{
+	return mCapacity - mHead;
+}
+
+template <typename T>
+void ReadMemoryStream::Read(T& outData)
+{
+	static_assert(std::is_arithmetic<T>::value || std::is_enum<T>::value, "Generic Read only supports primitive data types");
+	Read(&outData, sizeof(outData));
+}
